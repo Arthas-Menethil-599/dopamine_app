a65c6f467f356218165bcd36bb4e66c8351b4804
sirba <sir.baimyrza@gmail.com>
2022-05-31 02:10:43 +0200

    Finished 2nd lesson

---
+++ android/app/src/main/kotlin/com/example/dopamine_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dopamine_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              height: 200.0,
+            ),
+            Text(
+              'Значение счетчика:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            Expanded(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    height: 45.0,
+                    child: RawMaterialButton(
+                      fillColor: Colors.blue,
+                      onPressed: _incrementCounter,
+                      child: const Icon(
+                        Icons.add,
+                        color: Colors.white,
+                      ),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5.0)),
+                    ),
+                  ),
+                  Container(
+                    height: 45.0,
+                    child: RawMaterialButton(
+                      fillColor: Colors.blue,
+                      onPressed: _decrementCounter,
+                      child: const Icon(
+                        Icons.remove,
+                        color: Colors.white,
+                      ),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5.0)),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
